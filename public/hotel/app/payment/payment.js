5a1c9de5618dfd152f67c750403f76d9de168279
Dominique Miller <dominique.p.miller@gmail.com>
2016-04-04 22:21:19 -0400

    Finalize front-end of stripe integration

--- public/hotel/app/payment/payment.js
+++ public/hotel/app/payment/payment.js
@@ -2,15 +2,38 @@
 
 angular.module('myApp.payment', ['ngRoute'])
 
-      .config(['$routeProvider', function(routeProvider){
-        $routeProvider.when('/payment', {
+      .config(['$routeProvider', function($routeProvider){
+        $routeProvider.when('/payment/:reserve_id', {
             templateUrl: 'payment/payment.html',
             controller: 'PaymentController'
         });
       }])
 
-      .controller('PaymentController', function(){
+      .controller('PaymentController', function($scope, $http, ReservationService, $location, $routeParams){
 
+          $scope.res_id = $routeParams.reserve_id;
+          $scope.paid = false;
 
-        
+          $scope.paywithStripe = function(status, response){
+            if(response.error) {
+              $scope.paid = false;
+              $scope.message = "Yo shit don't work!";
+            } else {
+              var $payInfo = {
+                'token': response.id,
+                'customer_id': $scope.reservation_info.customer_id,
+                'total': $scope.reservation_info.total_price
+              };
+            }
+          };
+
+          $scope.getReservationData = function(){
+            $http.get('api/reservation/' + $scope.res_id).then(function(response){
+              $scope.reservation_info = response.data;
+              console.log($scope.reservation_info);
+            });
+          };
+
+          $scope.getReservationData();
+
       });
